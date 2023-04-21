e7126932f5923703fcf0cfcc35b05812e8e42697
Tharindu N <tharindu.nm@yahoo.com>
2023-04-21 08:20:18 +0530

    Some updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+import Homepage from "./components/homepage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Homepage />
+		</div>
+	);
 }
 
 export default App;
---
+++ frontend/src/components/homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage = () => {
+	return (
+		<React.Fragment>
+			<h1>Homepage</h1>
+		</React.Fragment>
+	);
+};
+
+export default Homepage;
